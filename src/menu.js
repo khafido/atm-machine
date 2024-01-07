b0143f02b5a67bdb9118c67384edcc7b9c1a5433
khafido <khafido.work@gmail.com>
2024-01-08 00:07:38 +0700

    done white belt

---
+++ src/menu.js
@@ -0,0 +1,14 @@
+// Function to display menu options
+function displayMenu() {
+    console.log("\n------------------------");
+    console.log("Menu Options:");
+    console.log("1. View account balance");
+    console.log("2. Withdraw cash");
+    console.log("3. Deposit cash");
+    console.log("4. Transfer funds");
+    console.log("5. Exit");
+}
+
+module.exports = {
+    displayMenu
+};
